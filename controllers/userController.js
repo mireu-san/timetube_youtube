7045f29791f6bc08596198b8d74c83a515266e02
mireu-san <seulbiclosers@gmail.com>
2021-02-19 21:09:03 +0900

    MVC pattern - import, export and minimise all mess by separating frequent code

---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("Log in");
+export const logout = (req, res) => res.send("Log out");
+export const users = (req, res) => res.send("Users");
+export const userDetail = (req, res) => res.send("User Detail");
+export const editProfile = (req, res) => res.send("Edit Profile");
+export const changePassword = (req, res) => res.send("Change Password");
